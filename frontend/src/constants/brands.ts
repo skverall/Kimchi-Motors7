f897b9442e7b22d80af0957ea9733b7552d641eb
AydMaxx <aydmaxx@gmail.com>
2025-11-21 20:40:50 +0400

    3к

--- frontend/src/constants/brands.ts
+++ frontend/src/constants/brands.ts
@@ -1,4 +1,5 @@
 export const BRANDS = [
+  // Luxury & Supercars
   {
     name: "Mercedes-Benz",
     logo: "https://logo.clearbit.com/mercedes-benz.com",
@@ -39,4 +40,120 @@ export const BRANDS = [
     logo: "https://logo.clearbit.com/rolls-roycemotorcars.com",
     models: ["Phantom", "Cullinan", "Ghost", "Wraith", "Dawn", "Spectre", "Boat Tail"]
   },
+
+  // Asian Brands
+  {
+    name: "Toyota",
+    logo: "https://logo.clearbit.com/toyota.com",
+    models: ["Camry", "Corolla", "RAV4", "Highlander", "Land Cruiser", "Supra", "Prius", "Sienna", "Tacoma", "Tundra"]
+  },
+  {
+    name: "Honda",
+    logo: "https://logo.clearbit.com/honda.com",
+    models: ["Civic", "Accord", "CR-V", "Pilot", "Odyssey", "HR-V", "Passport", "Ridgeline"]
+  },
+  {
+    name: "Lexus",
+    logo: "https://logo.clearbit.com/lexus.com",
+    models: ["RX", "ES", "LS", "NX", "GX", "LX", "IS", "LC", "RC"]
+  },
+  {
+    name: "Hyundai",
+    logo: "https://logo.clearbit.com/hyundai.com",
+    models: ["Elantra", "Sonata", "Tucson", "Santa Fe", "Palisade", "Kona", "Ioniq 5", "Ioniq 6"]
+  },
+  {
+    name: "Kia",
+    logo: "https://logo.clearbit.com/kia.com",
+    models: ["K5", "Stinger", "Telluride", "Sorento", "Sportage", "Carnival", "EV6", "Soul"]
+  },
+  {
+    name: "Genesis",
+    logo: "https://logo.clearbit.com/genesis.com",
+    models: ["G70", "G80", "G90", "GV70", "GV80", "GV60"]
+  },
+  {
+    name: "Nissan",
+    logo: "https://logo.clearbit.com/nissanusa.com",
+    models: ["Altima", "Maxima", "Rogue", "Pathfinder", "Armada", "GT-R", "Z", "Frontier"]
+  },
+  {
+    name: "Mazda",
+    logo: "https://logo.clearbit.com/mazdausa.com",
+    models: ["Mazda3", "CX-5", "CX-30", "CX-50", "CX-90", "MX-5 Miata"]
+  },
+  {
+    name: "Subaru",
+    logo: "https://logo.clearbit.com/subaru.com",
+    models: ["Outback", "Forester", "Crosstrek", "Ascent", "Impreza", "WRX", "BRZ"]
+  },
+
+  // American Brands
+  {
+    name: "Tesla",
+    logo: "https://logo.clearbit.com/tesla.com",
+    models: ["Model S", "Model 3", "Model X", "Model Y", "Cybertruck"]
+  },
+  {
+    name: "Ford",
+    logo: "https://logo.clearbit.com/ford.com",
+    models: ["F-150", "Mustang", "Explorer", "Bronco", "Escape", "Edge", "Expedition", "Maverick"]
+  },
+  {
+    name: "Chevrolet",
+    logo: "https://logo.clearbit.com/chevrolet.com",
+    models: ["Silverado", "Corvette", "Camaro", "Tahoe", "Suburban", "Equinox", "Traverse", "Blazer"]
+  },
+  {
+    name: "Jeep",
+    logo: "https://logo.clearbit.com/jeep.com",
+    models: ["Wrangler", "Grand Cherokee", "Cherokee", "Compass", "Gladiator", "Wagoneer"]
+  },
+  {
+    name: "Cadillac",
+    logo: "https://logo.clearbit.com/cadillac.com",
+    models: ["Escalade", "CT5", "CT4", "XT5", "XT6", "Lyriq", "Celestiq"]
+  },
+  {
+    name: "GMC",
+    logo: "https://logo.clearbit.com/gmc.com",
+    models: ["Sierra", "Yukon", "Terrain", "Acadia", "Canyon", "Hummer EV"]
+  },
+  {
+    name: "Dodge",
+    logo: "https://logo.clearbit.com/dodge.com",
+    models: ["Challenger", "Charger", "Durango", "Hornet"]
+  },
+  {
+    name: "Ram",
+    logo: "https://logo.clearbit.com/ramtrucks.com",
+    models: ["1500", "2500", "3500", "ProMaster"]
+  },
+
+  // European Brands
+  {
+    name: "Volkswagen",
+    logo: "https://logo.clearbit.com/vw.com",
+    models: ["Golf GTI", "Golf R", "Jetta", "Tiguan", "Atlas", "ID.4", "Arteon"]
+  },
+  {
+    name: "Volvo",
+    logo: "https://logo.clearbit.com/volvocars.com",
+    models: ["XC90", "XC60", "XC40", "S90", "S60", "V90", "V60", "C40"]
+  },
+  {
+    name: "Land Rover",
+    logo: "https://logo.clearbit.com/landrover.com",
+    models: ["Range Rover", "Range Rover Sport", "Range Rover Velar", "Range Rover Evoque", "Defender", "Discovery"]
+  },
+  {
+    name: "Jaguar",
+    logo: "https://logo.clearbit.com/jaguar.com",
+    models: ["F-TYPE", "F-PACE", "E-PACE", "I-PACE", "XF"]
+  },
+  {
+    name: "Mini",
+    logo: "https://logo.clearbit.com/miniusa.com",
+    models: ["Cooper", "Countryman", "Clubman", "Convertible"]
+  }
 ];
