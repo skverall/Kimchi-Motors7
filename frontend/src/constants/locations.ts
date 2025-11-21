b7bbc5557371f0c8446102e6945276a093a4b1c4
AydMaxx <aydmaxx@gmail.com>
2025-11-21 20:56:40 +0400

    3

--- frontend/src/constants/locations.ts
+++ frontend/src/constants/locations.ts
@@ -6,7 +6,7 @@ export const SHOWROOM_LOCATIONS = [
     address: "Sheikh Zayed Road, Exit 42, Dubai",
     phone: "+971 4 123 4567",
     hours: "9:00 AM - 9:00 PM",
-    image: "https://images.unsplash.com/photo-1512453979798-5ea9ba6a80f4?auto=format&fit=crop&q=80&w=800",
+    image: "/dubai-showroom.png",
     mapQuery: "Sheikh+Zayed+Road+Dubai",
   },
   {
@@ -16,7 +16,7 @@ export const SHOWROOM_LOCATIONS = [
     address: "Gangnam-gu, Teheran-ro, Seoul",
     phone: "+82 2 987 6543",
     hours: "10:00 AM - 8:00 PM",
-    image: "https://images.unsplash.com/photo-1526481280695-3c687fd543c0?auto=format&fit=crop&q=80&w=800",
+    image: "/seoul-showroom.png",
     mapQuery: "Gangnam-gu+Seoul",
   },
   {
@@ -26,7 +26,7 @@ export const SHOWROOM_LOCATIONS = [
     address: "221B Baker Street, London",
     phone: "+44 20 7123 4567",
     hours: "9:00 AM - 7:00 PM",
-    image: "https://images.unsplash.com/photo-1513635269975-59663e0ac1ad?auto=format&fit=crop&q=80&w=800",
+    image: "/london-showroom.png",
     mapQuery: "Park+Lane+London",
   },
 ];
