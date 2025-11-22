c4c20bbcfd9be315975a3990e08f46c2bad9eabc
AydMaxx <aydmaxx@gmail.com>
2025-11-22 19:41:41 +0400

    j

--- frontend/src/types/supabase.ts
+++ frontend/src/types/supabase.ts
@@ -71,19 +71,18 @@ export interface Database {
                     image_version?: number | null
                 }
             }
+            Views: {
+                [_ in never]: never
+            }
+            Functions: {
+                [_ in never]: never
+            }
+            Enums: {
+                [_ in never]: never
+            }
+            CompositeTypes: {
+                [_ in never]: never
+            }
         }
     }
-    Views: {
-        [_ in never]: never
-    }
-    Functions: {
-        [_ in never]: never
-    }
-    Enums: {
-        [_ in never]: never
-    }
-    CompositeTypes: {
-        [_ in never]: never
-    }
-}
 }
